a303eb993d6ef18e3765d21098e32a4e2a5a5460
Sagar Seth <139375536+lightning-sagar@users.noreply.github.com>
2024-02-28 03:03:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -639,6 +639,3 @@ const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
-listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
