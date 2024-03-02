07bfdf156252478500af4027011c8ae559f267dd
Sagar Seth <139375536+lightning-sagar@users.noreply.github.com>
2024-03-03 00:29:19 +0530

    Update todo.js

--- models/todo.js
+++ models/todo.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const todoSchema = new mongoose.Schema({
   text: String,
-  dateTime: Date,
+  dateTime: new Date(dateTime),
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',  
