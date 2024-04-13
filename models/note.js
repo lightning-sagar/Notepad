a1f959e9524d93289317b4889eb3624006b22ca8
Sagar Seth <139375536+lightning-sagar@users.noreply.github.com>
2024-04-13 15:05:07 +0530

    Update note.js

--- models/note.js
+++ models/note.js
@@ -1,22 +1,24 @@
 const mongoose = require('mongoose');
 
-const noteSchema = new mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
-  },
-  StartTime: Date,
-  EndTime: Date,
-  title: {
-    type: String,
-  },
-  completed: {
-    type:Boolean,
-    default: false
-  },
-  note: {
-    type: String,
+const noteSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    StartTime: Date,
+    EndTime: Date,
+    title: {
+      type: String,
+    },
+    completed: {
+      type: Boolean,
+      default: false,
+    },
+    note: {
+      type: String,
+    },
   },
   { timestamps: true }
 );
